bc484dfb3df63aa8dbfd8f5a96d0cdc835816981
SPC-Toad <sangyunkiment@gmail.com>
2024-04-01 01:00:41 -0400

    start of sprint3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Main from './pages/Main/Main';
-import Calendar from './pages/Calendar/Calendar';
 import { AuthContextProvider } from './context/AuthContext';
+import './App.css';
+
+import Main from './pages/Main/Main';
+import Account from './pages/Account/Account';
+import Todo from './pages/Todo/Todo';
 import Protected from './componets/Protected';
 
 function App() {
@@ -13,7 +15,8 @@ function App() {
         <AuthContextProvider>
           <Routes>
             <Route path='/' element={ <Main /> } />
-            <Route path='/calendar' element={ <Protected><Calendar /></Protected> } />
+            <Route path='/Account' element={ <Protected><Account /></Protected> } />
+            <Route path='/Todo' element={ <Protected><Todo /></Protected> } />
           </Routes>
         </AuthContextProvider>
       </Router>
