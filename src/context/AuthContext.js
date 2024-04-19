c7f69964c4141fe96abb6ccbce67c90fd3d90e8f
Foladipo1 <123270248+Foladipo1@users.noreply.github.com>
2024-04-18 20:34:17 -0400

    implemented fb user CRUD

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,21 +1,21 @@
 import { useContext, createContext, useEffect, useState } from "react";
 import {
     GoogleAuthProvider,
-    signInWithPopup, 
+    signInWithPopup,
     // signInWithRedirect, 
-    signOut, 
+    signOut,
     onAuthStateChanged,
 } from "firebase/auth";
 import { auth } from '../firebase'
-
+import { userExist, addUser, setUserOauthToken, updateUserOAuthToken } from '../firestore/firebaseUser.js'
 const AuthContext = createContext();
 
-export const AuthContextProvider = ({children}) => {
+export const AuthContextProvider = ({ children }) => {
     // Getter and Setter for storing user data
     const [user, setUser] = useState({});
     // Getter and Setter for storing user cred
-    const [ credentials, setCredentials] = useState({})
-    
+    const [credentials, setCredentials] = useState({})
+
     // Signin function 
     const googleSignIn = async () => {
         // Using google auth provider,
@@ -24,26 +24,36 @@ export const AuthContextProvider = ({children}) => {
         provider.addScope('https://www.googleapis.com/auth/calendar');
         // Pop up of all google accounts to choose 
         await signInWithPopup(auth, provider)
-        // Whatever the result is, as long as result is not null, set the cred
-        .then(result => {
-            // result._tokenResponse.oauthAccessToken
+            // Whatever the result is, as long as result is not null, set the cred
+            .then(async result => {
+                if (result != null) {
+                    const uid = result.user.uid;
+                    // console.log(uid)
+                    if (await userExist(uid)) {
+                        setUserOauthToken(uid, result._tokenResponse.oauthAccessToken);
+                        console.log("successfully updated token")
+                    }
+                    else {
+                        setUserOauthToken(uid, result._tokenResponse.oauthAccessToken);
+                        console.log("successfully added")
+                    }
+                }
+                // check if result is null or not
 
-            // check if result is null or not
+                // if it is not then you do the following:
 
-            // if it is not then you do the following:
-            
-            // if (!userExist(uid))
-                // then you can add user with oauthToken here
-            // Else
+                // if (!userExist(uid))
+                // then you can add user with oauthToken her
+                // Else
                 // the user exist so update user oauthToken here
 
 
-            result && setCredentials(result);
-        })
-        // Shit fails then throw error
-        .catch(error => {
-            console.error(error)
-        })
+                result && setCredentials(result);
+            })
+            // Shit fails then throw error
+            .catch(error => {
+                console.error(error)
+            })
     };
 
     // Just signout function
@@ -52,12 +62,12 @@ export const AuthContextProvider = ({children}) => {
     }
 
     // When the Auth changes, set the user as current auth
-    useEffect( () => {
+    useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             setUser(currentUser);
         });
-        return () => {unsubscribe()}
-    },[])
+        return () => { unsubscribe() }
+    }, [])
 
     // If you look at App.js, it is router container. anything enclosed by "AuthContextProvider" tag can access "googleSignIn, googleSignOut, user, credentials" functions/data!
     return (
--- src/firestore/firebaseUser.js
+++ src/firestore/firebaseUser.js
@@ -1,50 +1,102 @@
 import React from 'react'
-import { 
-    collection, 
-    getDocs, 
-    addDoc, 
-    deleteDoc, 
-    doc, 
-    updateDoc, 
+import {
+    collection,
+    getDocs,
+    addDoc,
+    deleteDoc,
+    doc,
     getDoc,
-    query, 
-    where 
+    query,
+    where,
+    onSnapshot,
+    updateDoc, 
+    setDoc
 } from 'firebase/firestore';
 import { db } from '../firebase.js'
-
+import {auth} from '../firebase.js'
 const userCollectionRef = collection(db, "user")
 
 
 
-export const userExist = async (uid) => {
-    const docRef = await getDocs(userCollectionRef,
-        {
-            //
+export const userExist = async ( uid ) => {
+    //querysnapshot gets all the docs from the collection
+    const querySnapshot = await getDocs(userCollectionRef);
+    // console.log(uid)
+    //loop through all the docs till we find the user's doc then return 
+    for (const docSnapshot of querySnapshot.docs)
+     {
+        // console.log("---------------------------")
+        // console.log(docSnapshot._document.data.value.mapValue.fields.uid === uid)
+        // console.log("---------------------------")
+        const userData = docSnapshot.data();
+        if (userData.uid === uid) {
+            console.log("reached")
+            return true;
         }
-    );
+    }
+    return false;
+    //return null if we didn't find any doc with matching uid
 }
+
+
 export const addUser = async (uid, oauthToken) => {
     const docRef = await addDoc(userCollectionRef,
         {
-            //
-        }
-    );
-}
-export const setUserOauthToken = async (uid, oauthToken) => {
-    const docRef = await updateDoc(userCollectionRef,
-        {
-            //
+            "uid": uid,
+            "OauthToken": oauthToken
         }
     );
 }
 
+
+export const setUserOauthToken = async (uid, oauthToken) => {
+
+    const userDocRef = doc(db, 'user', uid);
+    await setDoc(userDocRef,
+    {
+        "uid": uid, 
+        "OauthToken" : oauthToken
+    }, 
+    {
+        merge:true
+    });
+}
+
+
+
+
+// export const setUserOauthToken = async (uid, oauthToken) => {
+//     try {
+//         // Check if oauthToken is defined
+//         if (oauthToken === undefined) {
+//             throw new Error('OAuth token is undefined');
+//         }
+
+//         const userDocRef = doc(db, 'user', uid); // Assuming 'user' is your collection name
+//         await setDoc(userDocRef, { uid, OauthToken: oauthToken }); // Set the document with the new OAuth token
+//         console.log('User OAuth token updated successfully');
+//     } catch (error) {
+//         console.error('Error setting user OAuth token:', error);
+//         throw error;
+//     }
+// };
+
+
 export const getUserOauthToken = async (uid) => {
     const docRef = await getDoc(userCollectionRef,
         {
-            //
+            "uid": uid
         }
     );
+
 }
+
+
+
+
+
+
+
 
 // export const addNewDeck = async (uid: String, titleInput: String) => {
 //     const docRef = await addDoc(decksCollectionRef, {
