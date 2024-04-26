cbcad08742fcfe333b88f127eb06ed4e0fdae0fa
SPC-Toad <sangyunkiment@gmail.com>
2024-04-25 23:43:42 -0400

    checkpoint

--- src/componets/Navbar/Navbar.js
+++ src/componets/Navbar/Navbar.js
@@ -11,11 +11,20 @@ import Spinner from '../spinner/spinner';
 // Icons
 import { RxCalendar } from "react-icons/rx";
 import { MdChecklistRtl } from "react-icons/md";
+import { LuSettings } from "react-icons/lu";
 
 
 function Navbar() {
     const navigate = useNavigate();
-    const { user } = UserAuth();
+    const { user, googleSignOut } = UserAuth();
+
+    const handleSignOut = async () => {
+        try {
+          await googleSignOut();
+        } catch (error) {
+          console.error('Sign out error', error);
+        }
+      };
 
   return (
     <div className='account-menu-container'>
@@ -23,14 +32,24 @@ function Navbar() {
             <Spinner/>
             <div className='account-menu-user-greet-text'>Hello {user?.displayName},</div>
         </div>
-        <div className='account-menu-control-container'>
-            <div className='account-menu-control-child-container' onClick={() => navigate('/account')}>
-                <RxCalendar className='account-menu-control-child-icon'/>
-                <div>Calendar</div>
+        <div className='account-menu-space-between'>
+            <div className='account-menu-control-container'>
+                <div className='account-menu-control-child-container' onClick={() => navigate('/account')}>
+                    <RxCalendar className='account-menu-control-child-icon'/>
+                    <div>Calendar</div>
+                </div>
+                <div className='account-menu-control-child-container' onClick={() => navigate('/todo')}>
+                    <MdChecklistRtl className='account-menu-control-child-icon'/>
+                    <div>To do List</div>
+                </div>
             </div>
             <div className='account-menu-control-child-container' onClick={() => navigate('/todo')}>
-                <MdChecklistRtl className='account-menu-control-child-icon'/>
-                <div>To do List</div>
+                <LuSettings className='account-menu-control-child-icon'/>
+                {user?.displayName ? (
+                <div onClick={handleSignOut}>Logout!</div>
+                ) : (
+                    <a href='/'>Log In</a>
+                )}
             </div>
         </div>
     </div>
