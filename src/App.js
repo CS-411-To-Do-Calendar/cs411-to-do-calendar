122d2e501bf6e410c04e3358366f344032262715
SPC-Toad <sangyunkiment@gmail.com>
2024-04-25 21:43:44 -0400

    cleaner

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import Main from './pages/Main/Main';
 import Account from './pages/Account/Account';
 import Todo from './pages/Todo/Todo';
 import Protected from './componets/Protected';
-import ADA from './pages/ADA/ADA';
-import Setting from './pages/Setting/Setting';
 import Tutorial from './pages/Tutorial/Tutorial';
 
 function App() {
@@ -21,8 +19,6 @@ function App() {
             <Route path='/tutorial' element={ <Protected><Tutorial /></Protected> } />
             <Route path='/Account' element={ <Protected><Account /></Protected> } />
             <Route path='/Todo' element={ <Protected><Todo /></Protected> } />
-            <Route path='/chatbot' element={ <Protected><ADA /></Protected> } />
-            <Route path='/setting' element={ <Protected><Setting /></Protected> } />
           </Routes>
         </AuthContextProvider>
       </Router>
